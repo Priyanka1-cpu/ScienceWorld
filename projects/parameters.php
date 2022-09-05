9068e1ee608c914a7bd29c5abce3578ff8c52e6d
Priyanka1-cpu <112787702+Priyanka1-cpu@users.noreply.github.com>
2022-09-04 22:36:22 -0700

    Update parameters.php

--- projects/parameters.php
+++ projects/parameters.php
@@ -1,6 +1,3 @@
  <?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
-?> 
+
+print "Hello world, This is App1Ind";
